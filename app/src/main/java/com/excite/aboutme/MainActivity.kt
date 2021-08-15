bc4ea392ed7ac6b2df986250bb08c1c55b57eb9d
excitexcite <excitenew@gmail.com>
2021-08-15 14:16:37 +0300

    Add data binding options

--- app/src/main/java/com/excite/aboutme/MainActivity.kt
+++ app/src/main/java/com/excite/aboutme/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.excite.aboutme
 
+import android.content.Context
+import android.hardware.input.InputManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.excite.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+/*        findViewById<Button>(R.id.button_done).setOnClickListener {
+            addNickname(it)
+        }*/
+        binding.buttonDone.setOnClickListener {
+            addNickname(it)
+        }
     }
+
+    private fun addNickname(view: View) {
+//        val editText = findViewById<EditText>(R.id.nickname_edit)
+//        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        binding.apply {
+            binding.nicknameText.text = binding.nicknameEdit.text
+            invalidateAll()
+            binding.nicknameEdit.text = View.GONE
+            binding.buttonDone.text = View.GONE
+            binding.nicknameText.text = View.VISIBLE
+        }
+
+//        nicknameTextView.text = editText.text
+//        editText.visibility = View.GONE
+//        view.visibility = View.GONE
+//        nicknameTextView.visibility = View.VISIBLE
+
+    }
+
 }
